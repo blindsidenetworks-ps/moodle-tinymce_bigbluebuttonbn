e87c97a3040ed62cc346e880c3a27b2c59a9650e
jfederico <jesus@123it.ca>
2016-09-06 13:54:19 -0400

    v1.0-b1: Promoted release to b1

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 /**
  * TinyMCE text editor bigbluebuttonbn version file.
  *
- * @package    atto_bigbluebuttonbn
+ * @package    tinymce_bigbluebuttonbn
  * @author     Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
  * @copyright  2016 Blindside Networks Inc.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -11,9 +11,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016080100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016090400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;        // Requires this Moodle version.
 $plugin->component = 'tinymce_bigbluebuttonbn';  // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_ALPHA; // Maturity level. (ALPHA, BETA, RC, STABLE)
+$plugin->maturity = MATURITY_BETA; // Maturity level. (ALPHA, BETA, RC, STABLE)
 $plugin->release = '1.0';
 $plugin->dependencies = array( 'mod_bigbluebuttonbn' => 2016080100 );
